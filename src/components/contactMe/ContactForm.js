87cbf67117c3abe844fa70ddd81d97d0615c8a3f
CJ <cliftonjones.87@gmail.com>
2022-03-18 20:13:04 -0400

    feat: added required fields for contact/hire form

--- src/components/contactMe/ContactForm.js
+++ src/components/contactMe/ContactForm.js
@@ -12,12 +12,6 @@ import { addLocale } from 'primereact/api';
 
 import { Button } from 'primereact/button';
 
-// import { Toast } from 'primereact/toast';
-// import { FileUpload } from 'primereact/fileupload';
-// import { ProgressBar } from 'primereact/progressbar';
-// import { Tooltip } from 'primereact/tooltip';
-// import { Tag } from 'primereact/tag';
-
 export default class ContactForm extends React.Component {
   constructor(props) {
     super(props);
@@ -46,10 +40,8 @@ export default class ContactForm extends React.Component {
       phoneNumber: '',
       jobDetails: '',
       availability: '',
-      // dragAndDrop: {},
       budget: '',
       preferredContactMethod: '',
-      // totalSize: 0,
       loading: false,
     };
 
@@ -116,18 +108,6 @@ export default class ContactForm extends React.Component {
       today: 'Hoy',
       clear: 'Claro',
     });
-
-    //drag and drop
-    // this.onUpload = this.onUpload.bind(this);
-    // this.onTemplateUpload = this.onTemplateUpload.bind(this);
-    // this.onTemplateSelect = this.onTemplateSelect.bind(this);
-    // this.onTemplateRemove = this.onTemplateRemove.bind(this);
-    // this.onTemplateClear = this.onTemplateClear.bind(this);
-    // this.onBasicUpload = this.onBasicUpload.bind(this);
-    // this.onBasicUploadAuto = this.onBasicUploadAuto.bind(this);
-    // this.headerTemplate = this.headerTemplate.bind(this);
-    // this.itemTemplate = this.itemTemplate.bind(this);
-    // this.emptyTemplate = this.emptyTemplate.bind(this);
   }
 
   //calendar touch ui
@@ -173,157 +153,6 @@ export default class ContactForm extends React.Component {
     );
   }
 
-  //drag and drop
-  // onUpload() {
-  //   this.toast.show({
-  //     severity: 'info',
-  //     summary: 'Success',
-  //     detail: 'File Uploaded',
-  //   });
-  // }
-
-  // onTemplateSelect(e) {
-  //   let totalSize = this.state.totalSize;
-  //   e.files.forEach((file) => {
-  //     totalSize += file.size;
-  //   });
-
-  //   this.setState({
-  //     totalSize,
-  //   });
-  // }
-
-  // onTemplateUpload(e) {
-  //   let totalSize = 0;
-  //   e.files.forEach((file) => {
-  //     totalSize += file.size || 0;
-  //   });
-
-  //   this.setState(
-  //     {
-  //       totalSize,
-  //     },
-  //     () => {
-  //       this.toast.show({
-  //         severity: 'info',
-  //         summary: 'Success',
-  //         detail: 'File Uploaded',
-  //       });
-  //     }
-  //   );
-  // }
-
-  // onTemplateRemove(file, callback) {
-  //   this.setState(
-  //     (prevState) => ({
-  //       totalSize: prevState.totalSize - file.size,
-  //     }),
-  //     callback
-  //   );
-  // }
-
-  // onTemplateClear() {
-  //   this.setState({ totalSize: 0 });
-  // }
-
-  // onBasicUpload() {
-  //   this.toast.show({
-  //     severity: 'info',
-  //     summary: 'Success',
-  //     detail: 'File Uploaded with Basic Mode',
-  //   });
-  // }
-
-  // onBasicUploadAuto() {
-  //   this.toast.show({
-  //     severity: 'info',
-  //     summary: 'Success',
-  //     detail: 'File Uploaded with Auto Mode',
-  //   });
-  // }
-
-  // headerTemplate(options) {
-  //   const { className, chooseButton, uploadButton, cancelButton } = options;
-  //   const value = this.state.totalSize / 10000;
-  //   const formatedValue = this.fileUploadRef
-  //     ? this.fileUploadRef.formatSize(this.state.totalSize)
-  //     : '0 B';
-
-  //   return (
-  //     <div
-  //       className={className}
-  //       style={{
-  //         backgroundColor: 'transparent',
-  //         display: 'flex',
-  //         alignItems: 'center',
-  //       }}
-  //     >
-  //       {chooseButton}
-  //       {uploadButton}
-  //       {cancelButton}
-  //       <ProgressBar
-  //         value={value}
-  //         displayValueTemplate={() => `${formatedValue} / 10 MB`}
-  //         style={{ width: '300px', height: '20px', marginLeft: 'auto' }}
-  //       ></ProgressBar>
-  //     </div>
-  //   );
-  // }
-
-  // itemTemplate(file, props) {
-  //   return (
-  //     <div className="flex align-items-center flex-wrap">
-  //       <div className="flex align-items-center" style={{ width: '40%' }}>
-  //         <img
-  //           alt={file.name}
-  //           role="presentation"
-  //           value={this.state.dragAndDrop}
-  //           src={file.objectURL}
-  //           width={100}
-  //         />
-  //         <span className="flex flex-column text-left ml-3">
-  //           {file.name}
-  //           <small>{new Date().toLocaleDateString()}</small>
-  //         </span>
-  //       </div>
-  //       <Tag
-  //         value={props.formatSize}
-  //         severity="warning"
-  //         className="px-3 py-2"
-  //       />
-  //       <Button
-  //         type="button"
-  //         icon="pi pi-times"
-  //         className="p-button-outlined p-button-rounded p-button-danger ml-auto"
-  //         onClick={() => this.onTemplateRemove(file, props.onRemove)}
-  //       />
-  //     </div>
-  //   );
-  // }
-
-  // emptyTemplate() {
-  //   return (
-  //     <div className="flex align-items-center flex-column">
-  //       <i
-  //         className="pi pi-image mt-3 p-5"
-  //         style={{
-  //           fontSize: '5em',
-  //           borderRadius: '50%',
-  //           backgroundColor: 'var(--surface-b)',
-  //           color: 'var(--surface-d)',
-  //         }}
-  //       ></i>
-  //       <span
-  //         style={{ fontSize: '1.2em', color: 'var(--text-color-secondary)' }}
-  //         className="my-5"
-  //       >
-  //         Drag and Drop Image Here
-  //       </span>
-  //     </div>
-  //   );
-  // }
-
-  //submit loading
   onLoadingClick() {
     this.setState({ loading: true });
     setTimeout(() => {
@@ -331,27 +160,19 @@ export default class ContactForm extends React.Component {
     }, 2000);
   }
 
+  // onSubmitEvent() {
+  //   this.onLoadingClick();
+  //   const audio = new Audio(SlotMachineJackpot);
+  //   const formSubmit = () =>
+  //     'https://formsubmit.co/ed8588bae0bd9d8669f5533fa84f2957';
+  //   let playAudio = () => audio.play();
+  //   playAudio();
+  //   formSubmit();
+  // }
+
   //form render
 
   render() {
-    // const chooseOptions = {
-    //   icon: 'pi pi-fw pi-images',
-    //   iconOnly: true,
-    //   className: 'custom-choose-btn p-button-rounded p-button-outlined',
-    // };
-    // const uploadOptions = {
-    //   icon: 'pi pi-fw pi-cloud-upload',
-    //   iconOnly: true,
-    //   className:
-    //     'custom-upload-btn p-button-success p-button-rounded p-button-outlined',
-    // };
-    // const cancelOptions = {
-    //   icon: 'pi pi-fw pi-times',
-    //   iconOnly: true,
-    //   className:
-    //     'custom-cancel-btn p-button-danger p-button-rounded p-button-outlined',
-    // };
-
     return (
       <form
         action="https://formsubmit.co/ed8588bae0bd9d8669f5533fa84f2957"
@@ -384,6 +205,7 @@ export default class ContactForm extends React.Component {
                 onChange={(event) =>
                   this.setState({ firstName: event.target.value })
                 }
+                required
               />
               <label
                 htmlFor="firstName"
@@ -404,6 +226,7 @@ export default class ContactForm extends React.Component {
                 onChange={(event) =>
                   this.setState({ lastName: event.target.value })
                 }
+                required
               />
               <label
                 htmlFor="lastName"
@@ -431,6 +254,7 @@ export default class ContactForm extends React.Component {
                 onChange={(event) =>
                   this.setState({ email: event.target.value })
                 }
+                required
               />
               <label
                 htmlFor="email"
@@ -447,7 +271,6 @@ export default class ContactForm extends React.Component {
               style={{ colorScheme: 'red' }}
             >
               <i className="pi pi-phone" />
-              {/* <label htmlFor="phone">Phone</label> */}
               <InputMask
                 id="phone"
                 name="phoneNumber"
@@ -457,6 +280,7 @@ export default class ContactForm extends React.Component {
                 onChange={(event) =>
                   this.setState({ phoneNumber: event.value })
                 }
+                required
               />
             </span>
 
@@ -474,6 +298,7 @@ export default class ContactForm extends React.Component {
                 rows={5}
                 cols={30}
                 autoResize
+                required
               />
               <label
                 htmlFor="jobDetails"
@@ -494,6 +319,7 @@ export default class ContactForm extends React.Component {
                 onChange={(event) =>
                   this.setState({ preferredContactMethod: event.target.value })
                 }
+                required
               />
               <label
                 htmlFor="preferredContactMethod"
@@ -514,6 +340,7 @@ export default class ContactForm extends React.Component {
                 onChange={(event) =>
                   this.setState({ budget: event.target.value })
                 }
+                required
               />
               <label
                 htmlFor="budget"
@@ -555,6 +382,7 @@ export default class ContactForm extends React.Component {
                     showIcon
                     touchUI
                     showTime
+                    required
                   />
                 </div>
               </div>
@@ -562,69 +390,14 @@ export default class ContactForm extends React.Component {
 
             <div className="card"></div>
 
-            {/* <div>
-              <Toast
-                ref={(el) => {
-                  this.toast = el;
-                }}
-              ></Toast>
-
-              <Tooltip
-                target=".custom-choose-btn"
-                content="Choose"
-                position="bottom"
-              />
-              <Tooltip
-                target=".custom-upload-btn"
-                content="Upload"
-                position="bottom"
-              />
-              <Tooltip
-                target=".custom-cancel-btn"
-                content="Clear"
-                position="bottom"
-              /> */}
-
-            {/* <div className="card">
-                <h5 style={{ fontSize: '1em', color: 'silver' }}>
-                  Proposal/Offer Letter
-                </h5>
-                <FileUpload
-                  ref={(el) => (this.fileUploadRef = el)}
-                  name="dragAndDrop"
-                  url="/upload"
-                  multiple
-                  value={this.state.dragAndDrop}
-                  accept="image/*"
-                  maxFileSize={10000000}
-                  onUpload={this.onTemplateUpload}
-                  onSelect={this.onTemplateSelect}
-                  onError={this.onTemplateClear}
-                  onClear={this.onTemplateClear}
-                  headerTemplate={this.headerTemplate}
-                  itemTemplate={this.itemTemplate}
-                  emptyTemplate={this.emptyTemplate}
-                  chooseOptions={chooseOptions}
-                  uploadOptions={uploadOptions}
-                  cancelOptions={cancelOptions}
-                  onChange={(data) =>
-                    this.setState({ dragAndDrop: data.value })
-                  }
-                />
-              </div> */}
-            {/* </div> */}
-
-            <div className="card"></div>
-
             <div style={{ textAlign: 'center' }}>
               <Button
                 label="Hire Me"
                 loading={this.state.loading}
-                onClick={[
-                  () =>
-                    'https://formsubmit.co/ed8588bae0bd9d8669f5533fa84f2957',
-                  this.onLoadingClick,
-                ]}
+                // onClick={this.onSubmitEvent}
+                onClick={() =>
+                  'https://formsubmit.co/ed8588bae0bd9d8669f5533fa84f2957'
+                }
                 action="https://formsubmit.co/ed8588bae0bd9d8669f5533fa84f2957"
                 method="POST"
               />
