bd5c792f1c88fa526e187290ded3a761826f6f9a
CJ <cliftonjones.87@gmail.com>
2022-07-03 22:48:54 -0400

    fixed project gallery, reformatted using new package

--- src/components/projects/ProjectGallery.js
+++ src/components/projects/ProjectGallery.js
@@ -1,91 +1,107 @@
-import { Carousel } from 'react-carousel-minimal';
+import ImageGallery from 'react-image-gallery';
+import './Projects.css';
 
 import IdleClicker from './IdleClicker.png';
 import GuessingGame from './GuessingGame.png';
 import computerLove from './computerLove.gif';
 import BoundzBookstore from './BoundzBookstore.png';
 import Hypnotiq from './Hypnotiq.gif';
-import SeeJonesEngineer from './SeeJonesEngineer.png';
 // import imageNotFound from './imageNotFound.jpeg';
 
 export default function ProjectGallery() {
-  const data = [
+  const imageGallery = [
     {
-      image: computerLove,
-      caption: "Computer Love",
+      original: computerLove,
+      thumbnail: computerLove,
+
+      thumbnailLabel: 'Computer Love',
+      description: 'Computer Love',
+
+      originalHeight: '300vh',
+      originalwidth: '75vw',
+      thumbnailHeight: '100vh',
+      thumbnailwidth: '100vw',
     },
     {
-      image: Hypnotiq,
-      caption: "Hypnotiq",
+      original: Hypnotiq,
+      thumbnail: Hypnotiq,
+
+      thumbnailLabel: 'Hypnotiq',
+      description: 'Hypnotiq',
+
+      originalHeight: '300vh',
+      originalwidth: '75vw',
+      thumbnailHeight: '100vh',
+      thumbnailwidth: '100vw',
     },
     {
-      image: BoundzBookstore,
-      caption: "Boundz Bookstore",
+      original: BoundzBookstore,
+      thumbnail: BoundzBookstore,
+
+      thumbnailLabel: 'Boundz Bookstore',
+      description: 'Boundz Bookstore',
+
+      originalHeight: '300vh',
+      originalwidth: '75vw',
+      thumbnailHeight: '100vh',
+      thumbnailwidth: '100vw',
     },
     {
-      image: IdleClicker,
-      caption: "Idle Clicker",
+      original: IdleClicker,
+      thumbnail: IdleClicker,
+
+      thumbnailLabel: 'Idle Clicker',
+      description: 'Idle Clicker',
+
+      originalHeight: '300vh',
+      originalwidth: '75vw',
+      thumbnailHeight: '100vh',
+      thumbnailwidth: '100vw',
     },
     {
-      image: GuessingGame,
-      caption: "Guessing Game",
+      original: GuessingGame,
+      thumbnail: GuessingGame,
+
+      thumbnailLabel: 'Guessing Game',
+      description: 'Guessing Game',
+
+      originalHeight: '300vh',
+      originalwidth: '75vw',
+      thumbnailHeight: '100vh',
+      thumbnailwidth: '100vw',
     },
   ];
 
-  const captionStyle = {
-    fontSize: "2em",
-    fontWeight: "bold",
-  };
-  const slideNumberStyle = {
-    fontSize: "20px",
-    fontWeight: "bold",
-  };
   return (
     <div
       className="App bg-black-alpha-90"
       style={{
-        color: "white",
-        textShadow: "2px 2px 2px #01020E",
-        paddingTop: "1rem",
+        color: 'white',
+        textShadow: '2px 2px 2px #01020E',
+        paddingTop: '1rem',
         paddingBottom: 175,
       }}
     >
-      <h1 style={{ textAlign: "center", color: "rebeccapurple" }}>Projects</h1>
-      <div style={{ textAlign: "center" }}>
+      <h1 style={{ textAlign: 'center', color: 'rebeccapurple' }}>Projects</h1>
+      <div style={{ textAlign: 'center' }}>
         <div
           style={{
-            padding: "0 20px",
+            padding: '0 20px',
           }}
         >
-          <Carousel
-            data={data}
-            time={5000}
-            width="75vw"
-            height="50vh"
-            captionStyle={captionStyle}
-            radius="10px"
-            slideNumber={false}
-            slideNumberStyle={slideNumberStyle}
-            captionPosition="top"
-            automatic={true}
-            dots={true}
-            pauseIconColor="white"
-            pauseIconSize="40px"
-            slideBackgroundColor="rebeccapurple"
-            slideImageFit="contain"
-            thumbnails={true}
-            thumbnailWidth={"100vw"}
-            showNavBtn={true}
-            style={{
-              textAlign: "center",
-              justifyContent: "center",
-              justifyItems: "center",
-              maxWidth: "50vw",
-              maxHeight: "50vh",
-              margin: "40px  auto",
-              color: "white",
-              textShadow: "2px 2px 2px #01020E",
-            }}
+          <ImageGallery
+            items={imageGallery}
+            infinite={true}
+            lazyLoad={true}
+            showNav={true}
+            showThumbnails={true}
+            thumbnailPosition={'bottom'}
+            showFullscreenButton={true}
+            useTranslate3D={true}
+            showPlayButton={false}
+            showBullets={true}
+            showIndex={true}
+            autoPlay={true}
           />
         </div>
       </div>
