eca664116386e5acbd08829b010e6444d31e9a1b
CJ <cliftonjones.87@gmail.com>
2022-11-18 21:08:01 -0500

    feature: added Hypnotiq 2.0 info for project slideshow

--- src/components/projects/data/ProjectData.jsx
+++ src/components/projects/data/ProjectData.jsx
@@ -10,37 +10,42 @@ import {
 export const ProjectData = [
   {
     id: 1,
+    image: HypnotiqPic,
+    caption: 'Hypnotiq 2.0',
+    href: 'https://www.seejonesengineer.com/projects/hypnotiq2',
+  },
+  {
+    id: 2,
     image: ComputerLovePic,
     caption: 'Computer Love',
     href: 'https://www.seejonesengineer.com/projects/computerlove',
   },
-
   {
-    id: 2,
+    id: 3,
     image: HypnotiqPic,
     caption: 'Hypnotiq',
     href: 'https://www.seejonesengineer.com/projects/hypnotiq',
   },
   {
-    id: 3,
+    id: 4,
     image: IYKYK,
     caption: 'IYKYK',
     href: 'https://www.seejonesengineer.com/projects/iykyk',
   },
   {
-    id: 4,
+    id: 5,
     image: browseBooks,
     caption: 'Boundz Bookstore',
     href: 'https://www.seejonesengineer.com/projects/boundzbookstore',
   },
   {
-    id: 5,
+    id: 6,
     image: IdleClickerPic,
     caption: 'Idle Clicker',
     href: 'https://www.seejonesengineer.com/projects/idleclicker',
   },
   {
-    id: 6,
+    id: 7,
     image: GuessingGamePic,
     caption: 'Guessing Game',
     href: 'https://www.seejonesengineer.com/projects/guessinggame',
