14aea28700785223cbf46b555636fe9ae2957260
CJ <cliftonjones.87@gmail.com>
2022-12-30 22:13:22 -0500

    refactor: refactored from class to functional component to limit the need for multiple lifecycle events

--- src/components/projects/AllProjects.jsx
+++ src/components/projects/AllProjects.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './ProjectsCSS/Projects.css';
 import './ProjectsCSS/ProjectSlideshow.css';
@@ -24,91 +24,91 @@ import {
   imageNotFound,
 } from '../../photos/PhotoExports';
 
-export default class AllProjects extends React.Component {
-  constructor(props) {
-    super(props);
+const AllProjects = () => {
+  const [dimensions, setDimensions] = useState({
+    height: innerHeight,
+    width: innerWidth,
+  });
 
-    this.state = {
-      projects: [
-        {
-          name: 'Hypnotiq 2.0',
-          image: Hypnotiq2GIF,
-          category: 'React Native Mobile App',
-          difficulty: 10,
-          status: 'v3 in progress',
-          href: 'https://www.seejonesengineer.com/projects/hypnotiq2',
-        },
-        {
-          name: 'All I Know Is, IYKYK',
-          image: IYKYKhome,
-          category: 'React Web App',
-          difficulty: 8,
-          status: 'complete',
-          href: 'https://www.seejonesengineer.com/projects/iykyk',
-        },
-        {
-          name: 'See Jones Engineer',
-          image: portfolioPic,
-          category: 'React Web App',
-          difficulty: 9,
-          status: 'complete',
-          href: 'https://www.seejonesengineer.com/',
-        },
-        {
-          name: 'Computer Love',
-          image: ComputerLovePic,
-          category: 'React Native Mobile App',
-          difficulty: 10,
-          status: 'complete',
-          href: 'https://www.seejonesengineer.com/projects/computerlove',
-        },
-        {
-          name: 'Hypnotiq',
-          image: HypnotiqPic,
-          category: 'React Native Mobile App',
-          difficulty: 7,
-          status: 'v2.0 complete',
-          href: 'https://www.seejonesengineer.com/projects/hypnotiq',
-        },
-        {
-          name: 'Boundz Bookstore',
-          image: browseBooks,
-          category: 'React Web App',
-          difficulty: 10,
-          status: 'complete',
-          href: 'https://www.seejonesengineer.com/projects/boundzbookstore',
-        },
-        {
-          name: 'Idle Clicker',
-          image: IdleClickerPic,
-          category: 'Vanilla JS, HTML, CSS',
-          difficulty: 6,
-          status: 'v2 in progress',
-          href: 'https://www.seejonesengineer.com/projects/idleclicker',
-        },
-        {
-          name: 'Guessing Game',
-          image: GuessingGamePic,
-          category: 'Vanilla JS, HTML, CSS',
-          difficulty: 3,
-          status: 'complete',
-          href: 'https://www.seejonesengineer.com/projects/guessinggame',
-        },
-      ],
-    };
+  const [projects, setProjects] = useState([
+    {
+      name: 'Hypnotiq 2.0',
+      image: Hypnotiq2GIF,
+      category: 'React Native Mobile App',
+      difficulty: 10,
+      status: 'v3 in progress',
+      href: 'https://www.seejonesengineer.com/projects/hypnotiq2',
+    },
+    {
+      name: 'All I Know Is, IYKYK',
+      image: IYKYKhome,
+      category: 'React Web App',
+      difficulty: 8,
+      status: 'complete',
+      href: 'https://www.seejonesengineer.com/projects/iykyk',
+    },
+    {
+      name: 'See Jones Engineer',
+      image: portfolioPic,
+      category: 'React Web App',
+      difficulty: 9,
+      status: 'complete',
+      href: 'https://www.seejonesengineer.com/',
+    },
+    {
+      name: 'Computer Love',
+      image: ComputerLovePic,
+      category: 'React Native Mobile App',
+      difficulty: 10,
+      status: 'complete',
+      href: 'https://www.seejonesengineer.com/projects/computerlove',
+    },
+    {
+      name: 'Hypnotiq',
+      image: HypnotiqPic,
+      category: 'React Native Mobile App',
+      difficulty: 7,
+      status: 'v2.0 complete',
+      href: 'https://www.seejonesengineer.com/projects/hypnotiq',
+    },
+    {
+      name: 'Boundz Bookstore',
+      image: browseBooks,
+      category: 'React Web App',
+      difficulty: 10,
+      status: 'complete',
+      href: 'https://www.seejonesengineer.com/projects/boundzbookstore',
+    },
+    {
+      name: 'Idle Clicker',
+      image: IdleClickerPic,
+      category: 'Vanilla JS, HTML, CSS',
+      difficulty: 6,
+      status: 'v2 in progress',
+      href: 'https://www.seejonesengineer.com/projects/idleclicker',
+    },
+    {
+      name: 'Guessing Game',
+      image: GuessingGamePic,
+      category: 'Vanilla JS, HTML, CSS',
+      difficulty: 3,
+      status: 'complete',
+      href: 'https://www.seejonesengineer.com/projects/guessinggame',
+    },
+  ]);
 
-    this.projectFunctions = new ProjectFunctions();
-    this.imageBodyTemplate = this.imageBodyTemplate.bind(this);
-    this.ratingBodyTemplate = this.ratingBodyTemplate.bind(this);
-  }
+  const projectFunctions = new ProjectFunctions();
 
-  componentDidMount() {
-    this.projectFunctions
-      .getProjectsSmall()
-      .then((data) => this.setState({ projects: data }));
-  }
+  useEffect(() => {
+    projectFunctions.getProjectsSmall().then((data) => setProjects(data));
 
-  imageBodyTemplate(rowData) {
+    const handleResize = () =>
+      setDimensions({ height: innerWidth, width: innerWidth });
+
+    window.addEventListener('resize', handleResize);
+  }, [dimensions.width, dimensions.height]);
+
+  const imageBodyTemplate = (rowData) => {
     return (
       <a href={rowData.href} target="_blank">
         <img
@@ -121,61 +121,62 @@ export default class AllProjects extends React.Component {
         />
       </a>
     );
-  }
+  };
 
-  ratingBodyTemplate(rowData) {
+  const ratingBodyTemplate = (rowData) => {
     return (
       <Rating value={rowData.difficulty} readOnly cancel={false} stars={10} />
     );
-  }
+  };
 
-  render() {
-    const header = (
-      <div className="table-header">
-        See Jones Projects
-        <a href="/learningJournal" target="_blank">
-          <Button
-            icon="pi pi-images"
-            tooltip="view in my learning journal"
-            tooltipOptions={{ position: 'left' }}
-          />
-        </a>
-      </div>
-    );
-    const footer = `In total there are ${
-      this.state.projects ? this.state.projects.length : 0
-    } projects.`;
+  const header = (
+    <div className="table-header">
+      See Jones Projects
+      <a href="/learningJournal" target="_blank">
+        <Button
+          icon="pi pi-images"
+          tooltip="view in my learning journal"
+          tooltipOptions={{ position: 'left' }}
+        />
+      </a>
+    </div>
+  );
+  const footer = `In total there are ${
+    projects ? projects.length : 0
+  } projects.`;
 
-    return (
-      <div
-        className="datatable-project"
-        style={{
-          color: 'white',
-          textShadow: '2px 2px 2px #01020E',
-          paddingTop: '1rem',
-        }}
-      >
-        <div style={{ marginBottom: '2rem' }}>
-          <ProjectSlideshow slides={ProjectData} />
-        </div>
-        <div className="card">
-          <DataTable
-            value={this.state.projects}
-            header={header}
-            footer={footer}
-            responsiveLayout="scroll"
-          >
-            <Column field="name" header="Title"></Column>
-            <Column header="Image" body={this.imageBodyTemplate}></Column>
-            <Column field="category" header="Category"></Column>
-            <Column
-              field="rating"
-              header="Difficulty Level"
-              body={this.ratingBodyTemplate}
-            ></Column>
-          </DataTable>
-        </div>
+  return (
+    <div
+      className="datatable-project"
+      style={{
+        color: 'white',
+        textShadow: '2px 2px 2px #01020E',
+        paddingTop: '1rem',
+        width: dimensions.width,
+      }}
+    >
+      <div style={{ marginBottom: '2rem' }}>
+        <ProjectSlideshow slides={ProjectData} />
       </div>
-    );
-  }
-}
+      <div className="card">
+        <DataTable
+          value={projects}
+          header={header}
+          footer={footer}
+          responsiveLayout="scroll"
+        >
+          <Column field="name" header="Title"></Column>
+          <Column header="Image" body={imageBodyTemplate}></Column>
+          <Column field="category" header="Category"></Column>
+          <Column
+            field="rating"
+            header="Difficulty Level"
+            body={ratingBodyTemplate}
+          ></Column>
+        </DataTable>
+      </div>
+    </div>
+  );
+};
+
+export default AllProjects;
