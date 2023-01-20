6655f701748c32e6b44fae9817f5eb8c29842b4c
CJ <cliftonjones.87@gmail.com>
2023-01-20 17:04:15 -0500

    feature: updated business card

--- src/components/contactMe/BusinessCard.jsx
+++ src/components/contactMe/BusinessCard.jsx
@@ -58,67 +58,80 @@ const BusinessCard = () => {
             </div>
 
             <div className="business-card-phone-and-email">
-              <p>(929)</p>
+              <p>cj@seejonesengineer.com</p>
             </div>
 
             <div className="business-card-phone-and-email">
-              <p>cj@seejonesengineer.com</p>
+              <p>(929)</p>
             </div>
           </div>
         </div>
 
         <div className="business-card-back">
-          <div className="business-card-back-top">
-            <img src={BackLogo} />
-          </div>
-          <div className="business-card-back-bottom">
-            <a
-              href="https://www.seejonesengineer.com/assets/CJsSoftwareEngineeringResume.24a34476.pdf#"
-              target="_blank"
-            >
-              <i className="fa fa-file-pdf fa-lg white-text mr-md-5 mr-3 fa-2x" />
-            </a>
+          <div className="business-card-tech-links">
+            <div className="business-card-back-top">
+              <h2>CJ JONES</h2>
+              <a href="https://www.seejonesengineer.com/" target="_blank">
+                <h4>https://www.seejonesengineer.com/</h4>
+              </a>
+            </div>
 
-            <a
-              href="mailto:cj@seejonesengineer.com"
-              style={{ color: 'indigo', textDecoration: 'none' }}
-              target="_blank"
-            >
-              <i className="fa fa-at fa-lg white-text mr-md-5 mr-3 fa-2x" />
-            </a>
+            <div className="business-card-back-middle">
+              <img src={BackLogo} />
+            </div>
 
-            <a href="https://www.linkedin.com/in/cjones1827/" target="_blank">
-              <i className="fa fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x" />
-            </a>
+            <div className="business-card-back-bottom">
+              <a
+                href="https://www.seejonesengineer.com/assets/CJsSoftwareEngineeringResume.24a34476.pdf#"
+                target="_blank"
+              >
+                <i className="fa fa-file-pdf fa-lg white-text mr-md-5 mr-3 fa-2x" />
+              </a>
 
-            <a
-              href="https://www.instagram.com/seejonesengineer/"
-              target="_blank"
-            >
-              <i className="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x" />
-            </a>
+              <a
+                href="mailto:cj@seejonesengineer.com"
+                style={{ color: 'indigo', textDecoration: 'none' }}
+                target="_blank"
+              >
+                <i className="fa fa-at fa-lg white-text mr-md-5 mr-3 fa-2x" />
+              </a>
 
-            <a href="https://twitter.com/cjonesengineer1" target="_blank">
-              <i className="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x" />
-            </a>
+              <a href="https://www.linkedin.com/in/cjones1827/" target="_blank">
+                <i className="fa fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x" />
+              </a>
 
-            <a
-              href="https://www.youtube.com/channel/UCIgDLvCc7d6pX1LrhgwLZMw"
-              target="_blank"
-            >
-              <i className="fa fa-youtube fa-lg white-text mr-md-5 mr-3 fa-2x" />
-            </a>
+              <a
+                href="https://www.instagram.com/seejonesengineer/"
+                target="_blank"
+              >
+                <i className="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x" />
+              </a>
 
-            <a href="https://www.tiktok.com/@seejonesengineer" target="_blank">
-              <i className="fab fa-tiktok fa-lg white-text mr-md-5 mr-3 fa-2x" />
-            </a>
+              <a href="https://twitter.com/cjonesengineer1" target="_blank">
+                <i className="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x" />
+              </a>
 
-            <a
-              href="https://www.facebook.com/seejonesengineer/"
-              target="_blank"
-            >
-              <i className="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x" />
-            </a>
+              <a
+                href="https://www.youtube.com/channel/UCIgDLvCc7d6pX1LrhgwLZMw"
+                target="_blank"
+              >
+                <i className="fa fa-youtube fa-lg white-text mr-md-5 mr-3 fa-2x" />
+              </a>
+
+              <a
+                href="https://www.tiktok.com/@seejonesengineer"
+                target="_blank"
+              >
+                <i className="fab fa-tiktok fa-lg white-text mr-md-5 mr-3 fa-2x" />
+              </a>
+
+              <a
+                href="https://www.facebook.com/seejonesengineer/"
+                target="_blank"
+              >
+                <i className="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
