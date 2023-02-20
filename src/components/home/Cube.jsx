e38265ddda155a94b146a33be0fd0e8942fca52a
CJ <cliftonjones.87@gmail.com>
2023-02-19 19:06:10 -0500

    refactor: refactored to allow for dynamic resizing with browser window

--- src/components/home/Cube.jsx
+++ src/components/home/Cube.jsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 
 import Iframe from 'react-iframe';
 
-export default function Cube() {
+const Cube = () => {
+  const [dimensions, setDimensions] = useState({
+    height: innerHeight,
+    width: innerWidth,
+  });
+
+  useEffect(() => {
+    const handleResize = () =>
+      setDimensions({ height: innerHeight, width: innerWidth });
+
+    window.addEventListener('resize', handleResize);
+  }, [dimensions.height, dimensions.width]);
+
   return (
     <div
       style={{
         color: 'white',
         textShadow: '2px 2px 2px #01020E',
         paddingBottom: '2rem',
-        width: '100%',
+        width: dimensions.width,
       }}
     >
-      <div className="App" style={{ width: '100%' }}>
-        <Iframe
-          url="https://3d-box-animation.vercel.app/"
-          width="100%"
-          id="myId"
-          scrolling="no"
-          frameBorder={0}
-        />
+      <div className="App" style={{ width: dimensions.width }}>
+        <Suspense fallback={<p>Loading....</p>}>
+          <Iframe
+            url="https://3d-box-animation.vercel.app/"
+            width="100%"
+            id="myId"
+            scrolling="no"
+            frameBorder={0}
+          />
+        </Suspense>
       </div>
     </div>
   );
-}
+};
+
+export default Cube;
