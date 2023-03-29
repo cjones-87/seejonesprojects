860992e967cd21fe8d721f55178a8f026288a68a
CJ <cliftonjones.87@gmail.com>
2023-03-28 22:14:32 -0400

    refactor: refactored ui for project description from org chart to cards

--- src/components/projects/Hypnotiq2/Hypnotiq2.jsx
+++ src/components/projects/Hypnotiq2/Hypnotiq2.jsx
@@ -1,108 +1,101 @@
 import React, { Suspense, useEffect, useState } from 'react';
-import { OrganizationChart } from 'primereact/organizationchart';
-import { imageNotFound } from '../../../photos/PhotoExports';
 import Spinner from '../../../misc/Spinner';
+import handleImageFailure from '../../../misc/helpers/handleImageFailure';
 import Hypnotiq2Data from './Hypnotiq2Data';
+import { Card } from 'primereact/card';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const Hypnotiq2 = () => {
-  const [selection, setSelection] = useState([]);
   const [dimensions, setDimensions] = useState({
     height: innerHeight,
     width: innerWidth,
   });
 
-  const handleError = (event) => {
-    event.target.src = imageNotFound;
-    event.onerror = null;
-    return event;
-  };
-
   useEffect(() => {
     const handleResize = () =>
-      setDimensions({ height: innerHeight, width: innerWidth });
+      setDimensions({
+        height: innerHeight,
+        width: innerWidth,
+      });
 
     window.addEventListener('resize', handleResize);
   }, [dimensions.width, dimensions.height]);
 
-  const orgChart = Hypnotiq2Data;
-
-  const nodeTemplate = (node) => {
-    if (node.type) {
-      console.log(node);
-      return (
-        <div
-          className={
-            localStorage.getItem('lightMode') === 'true'
-              ? 'bg-black-alpha-00'
-              : 'bg-black-alpha-90'
-          }
-          style={{
-            color:
-              localStorage.getItem('lightMode') === 'true'
-                ? 'whitesmoke'
-                : 'rebeccapurple',
-            overflowWrap: 'anywhere',
-            width: dimensions.width / 9,
-          }}
-        >
-          <div className="node-header" style={{ fontSize: '1.5em' }}>
-            {node.label}
-          </div>
-          <div className="node-content">
-            <div>{node.data.name}</div>
-
-            <Suspense fallback={<Spinner />}>
+  return (
+    <>
+      {Hypnotiq2Data.map((item, index) => {
+        const header = (
+          <Suspense fallback={<Spinner />}>
+            {item.iframe ? (
               <iframe
-                allow="autoplay"
-                onError={handleError}
-                src={node.data.iframe}
+                height={dimensions.height / 4}
+                onError={handleImageFailure}
+                src={item.iframe}
                 style={{
                   border: 0,
                   borderRadius: 25,
+                  padding: 10,
                 }}
-                width={dimensions.width / 10}
+                width={dimensions.width}
               />
-            </Suspense>
+            ) : (
+              <LazyLoadImage
+                alt={item.alt}
+                effect="blur"
+                height={dimensions.height / 4}
+                onError={handleImageFailure}
+                src={item.img}
+                style={{
+                  borderRadius: 25,
+                  padding: 10,
+                }}
+                width={dimensions.width}
+              />
+            )}
+          </Suspense>
+        );
 
-            <div
-              style={{
-                color:
+        return (
+          <div
+            key={index}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: '2rem',
+            }}
+          >
+            <Suspense fallback={<Spinner />}>
+              <Card
+                className={
                   localStorage.getItem('lightMode') === 'true'
-                    ? 'whitesmoke'
-                    : 'rebeccapurple',
-              }}
-            >
-              {node.data.info}
-            </div>
+                    ? 'bg-black-alpha-20'
+                    : 'bg-black-alpha-90'
+                }
+                style={{
+                  borderRadius: 25,
+                  color:
+                    localStorage.getItem('lightMode') === 'true'
+                      ? 'rebeccapurple'
+                      : 'whitesmoke',
+                  textShadow:
+                    localStorage.getItem('lightMode') === 'true'
+                      ? '1px 1px 1px indigo'
+                      : '1px 1px 1px whitesmoke',
+                }}
+                header={header}
+                width={dimensions.width / 5}
+              >
+                <p className="m-0" style={{ textAlign: 'center' }}>
+                  {item.paragraph}
+                </p>
+              </Card>
+            </Suspense>
           </div>
-        </div>
-      );
-    }
-  };
-
-  return (
-    <div
-      className={
-        localStorage.getItem('lightMode') === 'true'
-          ? 'bg-black-alpha-20'
-          : 'bg-black-alpha-90'
-      }
-      style={{
-        alignItems: 'center',
-        width: dimensions.width,
-      }}
-    >
-      <div style={{ width: dimensions.width + 20 }}>
-        <OrganizationChart
-          nodeTemplate={nodeTemplate}
-          onSelectionChange={(event) => setSelection(event.data)}
-          selection={selection}
-          selectionMode="multiple"
-          style={{ overflow: 'scroll' }}
-          value={orgChart}
-        />
-      </div>
-    </div>
+        );
+      })}
+    </>
   );
 };
 
