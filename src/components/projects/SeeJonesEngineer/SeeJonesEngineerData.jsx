713efd09f3c9b9055385cddd73ae5be8cb4c20a3
CJ <cliftonjones.87@gmail.com>
2023-03-29 01:30:22 -0400

    refactor: updated project descriptions

--- src/components/projects/SeeJonesEngineer/SeeJonesEngineerData.jsx
+++ src/components/projects/SeeJonesEngineer/SeeJonesEngineerData.jsx
@@ -1,199 +1,30 @@
-import React from 'react';
-
 import {
   seeJonesEngineer,
-  aboutMe,
   landingPage,
   projects,
-  learningJournal,
   interactiveGames,
-  contactForm,
 } from '../../../photos/PhotoExports';
-
-import { FaReact } from 'react-icons/fa';
-
-import { ImHtmlFive } from 'react-icons/im';
-
-import { SiCss3, SiNetlify } from 'react-icons/si';
-
-import { DiJavascript1 } from 'react-icons/di';
 
 const SeeJonesEngineerData = [
   {
-    label: 'seejonesengineer.com',
-    className: 'title',
-    type: 'title',
-    expanded: true,
-    data: { avatar: seeJonesEngineer },
-    children: [
-      {
-        label: 'MVP Goal',
-        className: 'info',
-        type: 'info',
-        expanded: true,
-        data: { avatar: landingPage },
-        children: [
-          {
-            label: 'Minimum Viable Product',
-            className: 'moreInfo',
-            type: 'moreInfo',
-            expanded: false,
-            data: {
-              avatar: contactForm,
-              info: (
-                <div>
-                  <p>
-                    The goal of this portfolio is to provide current & future
-                    clients, companies, businesses, artist, & individuals with a
-                    showcase of my talents and personal work. Current &
-                    potential clients & employers are able peruse my works and
-                    reach out to schedule meetings/work.
-                  </p>
-                </div>
-              ),
-            },
-          },
-          {
-            label: 'Stretch Goals',
-            className: 'moreInfo',
-            type: 'moreInfo',
-            expanded: true,
-            data: {
-              avatar: projects,
-              info: (
-                <div>
-                  <p>
-                    Future goals for this site include continued monthly updates
-                    as well reworking the UI to make it more accessible for all.
-                    I would also like to add some 3-dimensional work in the
-                    future.
-                  </p>
-                </div>
-              ),
-            },
-          },
-        ],
-      },
-      {
-        label: 'Tech Stack',
-        className: 'info',
-        type: 'info',
-        expanded: true,
-        data: { avatar: learningJournal },
-        children: [
-          {
-            label: 'Technologies',
-            className: 'moreInfo',
-            type: 'moreInfo',
-            expanded: false,
-            data: {
-              avatar: landingPage,
-              info: (
-                <span>
-                  <DiJavascript1 />
-                  &nbsp;&nbsp;&nbsp;
-                  <FaReact />
-                  &nbsp;&nbsp;&nbsp;
-                  <ImHtmlFive />
-                  &nbsp;&nbsp;&nbsp;
-                  <SiCss3 />
-                  &nbsp;&nbsp;&nbsp;
-                  <i className="pi pi-prime" style={{ fontSize: '1em' }} />
-                  &nbsp;&nbsp;&nbsp;
-                  <SiNetlify />
-                </span>
-              ),
-            },
-          },
-          {
-            label: 'Integration',
-            className: 'moreInfo',
-            type: 'moreInfo',
-            expanded: true,
-            data: {
-              avatar: seeJonesEngineer,
-              info: (
-                <div>
-                  <p>
-                    seejonesengineer.com's front end was constructed & designed
-                    using&nbsp;&nbsp;&nbsp;
-                    <DiJavascript1 />
-                    &nbsp;&nbsp;&nbsp;
-                    <FaReact />
-                    &nbsp;&nbsp;&nbsp;
-                    <ImHtmlFive />
-                    &nbsp;&nbsp;&nbsp;
-                    <SiCss3 />
-                    &nbsp;&nbsp;&nbsp;
-                    <i className="pi pi-prime" style={{ fontSize: '1em' }} />
-                    &nbsp;&nbsp;&nbsp;
-                    <SiNetlify />
-                  </p>
-                </div>
-              ),
-            },
-          },
-        ],
-      },
-      {
-        label: 'Implementations',
-        className: 'info',
-        type: 'info',
-        expanded: true,
-        data: { avatar: projects },
-        children: [
-          {
-            label: 'Contributions',
-            className: 'moreInfo',
-            type: 'moreInfo',
-            expanded: false,
-            data: {
-              avatar: aboutMe,
-              info: (
-                <div>
-                  <p>
-                    This portfolio site was created solely by me. I used a
-                    combination of <SiCss3 />
-                    &nbsp;&&nbsp;
-                    <i className="pi pi-prime" style={{ fontSize: '1em' }} />
-                    &nbsp; to illustrate & enhance the UI/UX.
-                  </p>
-                </div>
-              ),
-            },
-          },
-          {
-            label: 'Challenges',
-            className: 'moreInfo',
-            type: 'moreInfo',
-            expanded: true,
-            data: {
-              avatar: interactiveGames,
-              info: (
-                <div>
-                  <p>
-                    The biggest challenge I ran into came early on in the
-                    development of this portfolio site. I ran into a bit of a
-                    stumbling block at first with routing internal pages to
-                    different routes. Even though I had routed pages a thousand
-                    times before, it took a deep dive of the internet for me to
-                    realize and understand how important it is to stay up on
-                    current documentation. I also learned a lesson on being
-                    explicitly specific concerning versions when searching for
-                    errors. A recent change in documentation removed some higher
-                    order components & basic internet searches would only return
-                    the previous versions of documentation. This initial
-                    struggle led to a vast improvement in my researching and
-                    debugging skills, as well as my overall internet search
-                    efficacy.
-                  </p>
-                </div>
-              ),
-            },
-          },
-        ],
-      },
-    ],
+    alt: 'SeeJonesEngineer Photo',
+    img: seeJonesEngineer,
+    paragraph: `I have developed a portfolio website, seejonesengineer.com, with the goal of showcasing my talents and personal work to current and potential clients, companies, businesses, artists, and individuals. The website's minimum viable product (MVP) is a user-friendly platform that allows users to browse my works and reach out to schedule meetings or work. The stretch goals for this website include continued monthly updates, improving the user interface (UI) to make it more accessible, and adding 3D work in the future.`,
+  },
+  {
+    alt: 'SeeJonesEngineer Photo',
+    img: landingPage,
+    paragraph: `The tech stack used to construct and design seejonesengineer.com's front-end includes JavaScript, React, HTML5, CSS3, PrimeReact, Three.js, and was deployed via Netlify. I integrated these technologies to create an interactive and visually appealing website that engages users and meets their needs. The website's technologies were carefully chosen to ensure a responsive, efficient, and intuitive UI/UX.`,
+  },
+  {
+    alt: 'SeeJonesEngineer Photo',
+    img: projects,
+    paragraph: `As the sole developer of this website, I contributed to all aspects of its creation, from concept to deployment. I used a combination of CSS3 and PrimeReact to illustrate and enhance the UI/UX, making it visually appealing, responsive, and interactive. While developing the website, I initially faced a significant challenge with routing internal pages to different routes. I had to conduct extensive research to understand the importance of staying up-to-date with current documentation and being explicit about versioning when searching for errors. This challenge helped me improve my researching and debugging skills, as well as my overall internet search efficacy.`,
+  },
+  {
+    alt: 'SeeJonesEngineer Photo',
+    img: interactiveGames,
+    paragraph: `In conclusion, seejonesengineer.com is an impressive portfolio website that showcases my talents and personal work while meeting the needs of current and potential clients. This website is user-friendly and allows for easy browsing of my projects and multiple ways to contact me for work. Stretch goals for the website include continued monthly updates, improving the user interface, and adding 3D work in the future. The website's tech stack, including JavaScript, React, HTML5, CSS3, PrimeReact, and deployed via Netlify, was carefully chosen to ensure a responsive, efficient, and intuitive UI/UX.`,
   },
 ];
 
