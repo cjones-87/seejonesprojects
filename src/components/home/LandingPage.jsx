a82407133a13fc198a1743df73ef9994d035c235
CJ <cliftonjones.87@gmail.com>
2023-04-03 17:53:30 -0400

    refactor: mapping through data instead of hard coding

--- src/components/home/LandingPage.jsx
+++ src/components/home/LandingPage.jsx
@@ -5,15 +5,23 @@ import Typewriter from 'typewriter-effect';
 import smoke from '../../photos/smoke.png';
 import OpenUp from '../../sounds/OpenUp.mp3';
 import AccessGrantedComputerVoice from '../../sounds/AccessGrantedComputerVoice.mp3';
+import {
+  BackendData,
+  FrontendData,
+  LanguagesData,
+  ToolsData,
+} from '../techSkills/TechSkillsData';
 import { Button } from 'primereact/button';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/mdc-dark-deeppurple/theme.css';
 import 'primereact/resources/primereact.css';
 import './LandingPage.css';
+import {
+  MobileDevelopmentData,
+  WebDevelopmentData,
+} from '../techSkills/SpecializationData';
 
 const LandingPage = () => {
-  console.log(localStorage);
-
   const [dimensions, setDimensions] = useState({
     height: innerHeight,
     width: innerWidth,
@@ -214,26 +222,23 @@ const LandingPage = () => {
                   border: 'none',
                 }}
               >
-                <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
-                    alt="react"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a
-                  href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-                    alt="javascript"
-                    width="40"
-                    height="40"
-                  />
-                </a>
+                {WebDevelopmentData.map((data, index) => (
+                  <a
+                    href={data.href}
+                    key={index}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      alt={data.alt}
+                      height="40"
+                      key={index}
+                      src={data.src}
+                      width="40"
+                    />
+                  </a>
+                ))}
+
                 <Typewriter
                   options={{
                     strings: 'Web Development',
@@ -254,39 +259,23 @@ const LandingPage = () => {
               </div>
 
               <div className="sectionD2">
-                <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
-                    alt="react"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a
-                  href="https://docs.expo.dev/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://i.imgur.com/LNiblbi.png"
-                    alt="react native expo"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                &nbsp;
-                <a
-                  href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-                    alt="javascript"
-                    width="40"
-                    height="40"
-                  />
-                </a>
+                {MobileDevelopmentData.map((data, index) => (
+                  <a
+                    href={data.href}
+                    key={index}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      alt={data.alt}
+                      height="40"
+                      key={index}
+                      src={data.src}
+                      width="40"
+                    />
+                  </a>
+                ))}
+
                 <Typewriter
                   options={{
                     strings: 'Mobile Development',
@@ -327,42 +316,22 @@ const LandingPage = () => {
                 style={{ width: dimensions.width / 3 }}
               >
                 <h4 align="center"> Proficient Languages </h4>
-                <a
-                  href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-                    alt="javascript"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a
-                  href="https://www.w3.org/html/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
-                    alt="html5"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a
-                  href="https://www.w3schools.com/css/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
-                    alt="css3"
-                    width="40"
-                    height="40"
-                  />
-                </a>
+                {LanguagesData.map((data, index) => (
+                  <a
+                    href={data.href}
+                    key={index}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      alt={data.alt}
+                      height="40"
+                      key={index}
+                      src={data.src}
+                      width="40"
+                    />
+                  </a>
+                ))}
               </div>
 
               <div
@@ -412,131 +381,22 @@ const LandingPage = () => {
                 style={{ width: dimensions.width / 3 }}
               >
                 <h4 align="center"> Front-End </h4>
-                <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
-                    alt="react"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a
-                  href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-                    alt="javascript"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                &nbsp;
-                <a
-                  href="https://www.typescriptlang.org/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
-                    alt="typescript"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a
-                  href="https://www.w3.org/html/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
-                    alt="html5"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a
-                  href="https://www.w3schools.com/css/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
-                    alt="css3"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a href="https://mui.com/" target="_blank" rel="noreferrer">
-                  <img
-                    src="https://seeklogo.com/images/M/material-ui-logo-5BDCB9BA8F-seeklogo.com.png"
-                    alt="MUI"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a
-                  href="https://www.primefaces.org/primereact/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://www.primefaces.org/primereact/images/primereact-logo-dark.svg"
-                    alt="PrimeReact UI"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a
-                  href="https://chakra-ui.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://img.icons8.com/color/344/chakra-ui.png"
-                    alt="Chakra UI"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a href="https://redux.js.org" target="_blank" rel="noreferrer">
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"
-                    alt="redux"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a
-                  href="https://webpack.js.org"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/d00d0969292a6569d45b06d3f350f463a0107b0d/icons/webpack/webpack-original-wordmark.svg"
-                    alt="webpack"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a href="https://babeljs.io/" target="_blank" rel="noreferrer">
-                  <img
-                    src="https://img.icons8.com/wired/344/ffffff/babel.png"
-                    alt="babel"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-                <a href="https://threejs.org/" target="_blank" rel="noreferrer">
-                  <img
-                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/threejs/threejs-original-wordmark.svg"
-                    alt="three js"
-                    width="40"
-                    height="40"
-                  />
-                </a>
+                {FrontendData.map((data, index) => (
+                  <a
+                    href={data.href}
+                    key={index}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      alt={data.alt}
+                      height="40"
+                      key={index}
+                      src={data.src}
+                      width="40"
+                    />
+                  </a>
+                ))}
               </div>
 
               <div
@@ -545,58 +405,22 @@ const LandingPage = () => {
               >
                 <h4 align="center"> Tools </h4>
 
-                <a
-                  href="https://www.netlify.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://img.icons8.com/external-tal-revivo-filled-tal-revivo/344/external-netlify-a-cloud-computing-company-that-offers-hosting-and-serverless-backend-services-for-static-websites-logo-filled-tal-revivo.png"
-                    alt="netlify"
-                    width="30"
-                    height="25"
-                  />
-                </a>
-                <a href="https://vercel.com/" target="_blank" rel="noreferrer">
-                  <img
-                    align="center"
-                    src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/344/ffffff/external-glyph-shapes-tanah-basah-glyph-tanah-basah-69.png"
-                    width="35"
-                    height="35"
-                    alt="vercel"
-                  />
-                </a>
-                <a href="https://github.com/" target="_blank" rel="noreferrer">
-                  <img
-                    align="center"
-                    src="https://img.icons8.com/dusk/45/000000/github.png"
-                    width="40"
-                    height="40"
-                    alt="github"
-                  />
-                </a>
-                <a href="https://git-scm.com/" target="_blank" rel="noreferrer">
-                  <img
-                    align="center"
-                    src="https://img.icons8.com/nolan/45/git.png"
-                    width="40"
-                    height="40"
-                    alt="git"
-                  />
-                </a>
-                <a
-                  href="https://www.heroku.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    align="center"
-                    src="https://img.icons8.com/nolan/45/heroku.png"
-                    width="40"
-                    height="40"
-                    alt="heroku"
-                  />
-                </a>
+                {ToolsData.map((data, index) => (
+                  <a
+                    href={data.href}
+                    key={index}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      alt={data.alt}
+                      height="40"
+                      key={index}
+                      src={data.src}
+                      width="40"
+                    />
+                  </a>
+                ))}
               </div>
               <div
                 className="sectionC2"
@@ -604,92 +428,22 @@ const LandingPage = () => {
               >
                 <h4 align="center"> Back-End </h4>
 
-                <a
-                  href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-                    alt="javascript"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-
-                <a href="https://nodejs.org" target="_blank" rel="noreferrer">
-                  <img
-                    src="https://img.icons8.com/fluency/344/node-js.png"
-                    alt="nodejs"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-
-                <a
-                  href="https://sequelize.org/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sequelize/sequelize-original-wordmark.svg"
-                    alt="sequelize"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-
-                <a
-                  href="https://www.postgresql.org"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
-                    alt="postgresql"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-
-                <a
-                  href="https://expressjs.com"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
-                    alt="express"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-
-                <a
-                  href="https://firebase.google.com/docs"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://img.icons8.com/color/2x/firebase.png"
-                    alt="firebase"
-                    width="40"
-                    height="40"
-                  />
-                </a>
-
-                <a
-                  href="https://firebase.google.com/docs/firestore"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="https://img.icons8.com/color/2x/cloud-firestore.png"
-                    alt="firestore"
-                    width="40"
-                    height="40"
-                  />
-                </a>
+                {BackendData.map((data, index) => (
+                  <a
+                    href={data.href}
+                    key={index}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      alt={data.alt}
+                      height="40"
+                      key={index}
+                      src={data.src}
+                      width="40"
+                    />
+                  </a>
+                ))}
               </div>
             </div>
 
