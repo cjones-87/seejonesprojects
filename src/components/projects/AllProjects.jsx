d3cb8b84215197ffd5fbc2e0a297c271e2a4f778
CJ <cliftonjones.87@gmail.com>
2023-04-03 08:52:25 -0400

    refactor: relocated project data for modularization

--- src/components/projects/AllProjects.jsx
+++ src/components/projects/AllProjects.jsx
@@ -1,17 +1,15 @@
 import React, { Suspense, useEffect, useState } from 'react';
-import {
-  IdleClickerPic,
-  GuessingGamePic,
-  browseBooks,
-  portfolioPic,
-} from '../../photos/PhotoExports';
 import handleImageFailure from '../../misc/helpers/handleImageFailure';
 import ProjectSlideshow from './ProjectSlideshow';
 import Spinner from '../../misc/Spinner';
+import {
+  header,
+  footer,
+  projects,
+  ratingBodyTemplate,
+} from './data/ProjectData';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { Button } from 'primereact/button';
-import { Rating } from 'primereact/rating';
 import { ProjectData } from './data/ProjectData';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
@@ -23,145 +21,6 @@ const AllProjects = () => {
     height: innerHeight,
     width: innerWidth,
   });
-
-  const projects = [
-    {
-      id: 0,
-      name: (
-        <a
-          href={'/projects/hypnotiq2'}
-          style={{ color: 'whitesmoke', textDecoration: 'none' }}
-          target="_blank"
-        >
-          Hypnotiq 2.0
-        </a>
-      ),
-      iframe: 'https://www.youtube.com/embed/GFI7VYjWjaY',
-      category: 'React Native Mobile App',
-      difficulty: 10,
-      status: 'v3 in progress',
-      href: '/projects/hypnotiq2',
-    },
-    {
-      id: 1,
-      name: (
-        <a
-          href={'/projects/iykyk'}
-          style={{ color: 'whitesmoke', textDecoration: 'none' }}
-          target="_blank"
-        >
-          All I Know Is, IYKYK
-        </a>
-      ),
-      iframe: 'https://www.youtube.com/embed/dRMjRk_XCQg',
-      category: 'React Web App',
-      difficulty: 8,
-      status: 'complete',
-      href: '/projects/iykyk',
-    },
-    {
-      id: 2,
-      name: (
-        <a
-          href={'/projects/seejonesengineer'}
-          style={{ color: 'whitesmoke', textDecoration: 'none' }}
-          target="_blank"
-        >
-          See Jones Engineer
-        </a>
-      ),
-      image: portfolioPic,
-      category: 'React Web App',
-      difficulty: 9,
-      status: 'complete',
-      href: '/',
-    },
-    {
-      id: 3,
-      name: (
-        <a
-          href={'/projects/computerlove'}
-          style={{ color: 'whitesmoke', textDecoration: 'none' }}
-          target="_blank"
-        >
-          Computer Love
-        </a>
-      ),
-      iframe: 'https://www.youtube.com/embed/r6beMntr7nQ',
-      category: 'React Native Mobile App',
-      difficulty: 10,
-      status: 'complete',
-      href: '/projects/computerlove',
-    },
-    {
-      id: 4,
-      name: (
-        <a
-          href={'/projects/hypnotiq'}
-          style={{ color: 'whitesmoke', textDecoration: 'none' }}
-          target="_blank"
-        >
-          Hypnotiq
-        </a>
-      ),
-      iframe: 'https://www.youtube.com/embed/Z-RTFL_FYu8',
-      category: 'React Native Mobile App',
-      difficulty: 7,
-      status: 'v2.0 complete',
-      href: '/projects/hypnotiq',
-    },
-    {
-      id: 5,
-      name: (
-        <a
-          href={'/projects/boundzbookstore'}
-          style={{ color: 'whitesmoke', textDecoration: 'none' }}
-          target="_blank"
-        >
-          Boundz Bookstore
-        </a>
-      ),
-      image: browseBooks,
-      category: 'React Web App',
-      difficulty: 10,
-      status: 'complete',
-      href: '/projects/boundzbookstore',
-    },
-    {
-      id: 6,
-      name: (
-        <a
-          href={'/projects/idleclicker'}
-          style={{ color: 'whitesmoke', textDecoration: 'none' }}
-          target="_blank"
-        >
-          Idle Clicker
-        </a>
-      ),
-      image: IdleClickerPic,
-      category: 'Vanilla JS, HTML, CSS',
-      difficulty: 6,
-      status: 'v2 in progress',
-      href: '/projects/idleclicker',
-    },
-    {
-      id: 7,
-      name: (
-        <a
-          href={'/projects/guessinggame'}
-          style={{ color: 'whitesmoke', textDecoration: 'none' }}
-          target="_blank"
-        >
-          Guessing Game
-        </a>
-      ),
-      image: GuessingGamePic,
-      category: 'Vanilla JS, HTML, CSS',
-      difficulty: 3,
-      status: 'complete',
-      href: 'projects/guessinggame',
-    },
-  ];
 
   useEffect(() => {
     const handleResize = () =>
@@ -211,28 +70,6 @@ const AllProjects = () => {
       </a>
     );
   };
-
-  const ratingBodyTemplate = (rowData) => {
-    return (
-      <Rating value={rowData.difficulty} readOnly cancel={false} stars={10} />
-    );
-  };
-
-  const header = (
-    <div className="table-header">
-      See Jones Projects
-      <a href="/learningJournal" target="_blank">
-        <Button
-          icon="pi pi-images"
-          tooltip="View in my learning journal"
-          tooltipOptions={{ position: 'left' }}
-        />
-      </a>
-    </div>
-  );
-  const footer = `In total there are ${
-    projects ? projects.length : 0
-  } projects.`;
 
   return (
     <div
