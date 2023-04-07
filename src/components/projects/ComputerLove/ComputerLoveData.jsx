ef2d2cd3ac9e7a1833f1c4bff79105db6ea62654
CJ <cliftonjones.87@gmail.com>
2023-04-07 12:24:24 -0400

    chore: fixed typo

--- src/components/projects/ComputerLove/ComputerLoveData.jsx
+++ src/components/projects/ComputerLove/ComputerLoveData.jsx
@@ -2,22 +2,22 @@ import { registrationScreen } from '../../../photos/PhotoExports';
 
 const ComputerLoveData = [
   {
-    alt: 'IYKYK Youtube Video',
+    alt: 'Computer Love Youtube Video',
     iframe: 'https://www.youtube.com/embed/r6beMntr7nQ',
     paragraph: `I led a group project in building a mobile application named Computer Love. The application is designed for users in the tech industry to register/login for dating/networking and view profiles of other users. The Minimum Viable Product (MVP) goal was to allow users to swipe yes or no to dating/networking based on their preferences. The application was built utilizing a PERN stack with React Native Expo and Redux. For the back-end, we used Node.js, PostgreSQL, Sequelize, and Express.`,
   },
   {
-    alt: 'IYKYK Youtube Video',
+    alt: 'Computer Love Youtube Video',
     iframe: 'https://www.youtube.com/embed/r6beMntr7nQ',
     paragraph: `In addition to the MVP, we had several stretch goals, including allowing users to deactivate/reactivate their account at their leisure and report other users for inappropriate behavior. More future plans would include users being able to add multiple photos and matching other users based on location.`,
   },
   {
-    alt: 'IYKYK Youtube Video',
+    alt: 'Computer Love Youtube Video',
     iframe: 'https://www.youtube.com/embed/r6beMntr7nQ',
     paragraph: `One of my main contributions to the project was building the initial back-end and front-end as separate repositories. I also designed and implemented the user matching/swiping logic, which required extensive research to understand that I needed to approach it as rotational interpolation and not as a singular directional movement.`,
   },
   {
-    alt: 'IYKYK Youtube Video',
+    alt: 'Computer Love Youtube Video',
     iframe: 'https://www.youtube.com/embed/r6beMntr7nQ',
     img: registrationScreen,
     paragraph: `One of the biggest challenges we faced was integrating the front-end with the back-end. We had to ensure that the two were communicating with each other constantly. Additionally, implementing the user matching/swiping logic was challenging due to the complexities of the algorithms required to make it work.`,
