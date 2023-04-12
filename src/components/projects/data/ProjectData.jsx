8cf54cf338cff9a909777b626a3ad29d8e03af6b
CJ <cliftonjones.87@gmail.com>
2023-04-12 00:16:55 -0400

    refactor: removed all a elements to remove error concerning nested a elements, removed hrefs

--- src/components/projects/data/ProjectData.jsx
+++ src/components/projects/data/ProjectData.jsx
@@ -12,43 +12,36 @@ export const ProjectData = [
     id: 1,
     iframe: 'https://www.youtube.com/embed/GFI7VYjWjaY',
     caption: 'Hypnotiq 2.0',
-    href: 'https://www.seejonesengineer.com/projects/hypnotiq2',
   },
   {
     id: 2,
     iframe: 'https://www.youtube.com/embed/dRMjRk_XCQg',
     caption: 'IYKYK',
-    href: 'https://www.seejonesengineer.com/projects/iykyk',
   },
   {
     id: 3,
     iframe: 'https://www.youtube.com/embed/r6beMntr7nQ',
     caption: 'Computer Love',
-    href: 'https://www.seejonesengineer.com/projects/computerlove',
   },
   {
     id: 4,
     iframe: 'https://www.youtube.com/embed/Z-RTFL_FYu8',
     caption: 'Hypnotiq',
-    href: 'https://www.seejonesengineer.com/projects/hypnotiq',
   },
   {
     id: 5,
     image: browseBooks,
     caption: 'Boundz Bookstore',
-    href: 'https://www.seejonesengineer.com/projects/boundzbookstore',
   },
   {
     id: 6,
     image: IdleClickerPic,
     caption: 'Idle Clicker',
-    href: 'https://www.seejonesengineer.com/projects/idleclicker',
   },
   {
     id: 7,
     image: GuessingGamePic,
     caption: 'Guessing Game',
-    href: 'https://www.seejonesengineer.com/projects/guessinggame',
   },
 ];
 
@@ -200,13 +193,12 @@ export const ratingBodyTemplate = (rowData) => {
 export const header = (
   <div className="table-header">
     See Jones Projects
-    <a href="/learningJournal" target="_blank">
-      <Button
-        icon="pi pi-images"
-        tooltip="View in my learning journal"
-        tooltipOptions={{ position: 'left' }}
-      />
-    </a>
+    <Button
+      id="navigateToLearningJournal"
+      icon="pi pi-images"
+      tooltip="View in my learning journal"
+      tooltipOptions={{ position: 'left' }}
+    />
   </div>
 );
 
