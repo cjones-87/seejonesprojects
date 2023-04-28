cb2c8ba9d4848101f2ca65773e48ca53dfec120f
CJ <cliftonjones.87@gmail.com>
2023-04-27 22:26:37 -0400

    style: made font and image sizes responsive

--- src/components/techSkills/TechSkills.jsx
+++ src/components/techSkills/TechSkills.jsx
@@ -12,51 +12,69 @@ export default function TechSkills() {
       style={{
         color:
           localStorage.getItem('lightMode') === 'true' ? 'indigo' : 'white',
+        fontSize: '1vmin',
         textShadow:
           localStorage.getItem('lightMode') === 'true'
             ? '2px 2px 2px whitesmoke'
             : '2px 2px 2px indigo',
+        paddingBottom: '2rem',
         paddingTop: '1rem',
       }}
     >
-      <h4 align="center">
+      <h4 align="center" style={{ fontSize: '3em' }}>
         <b>Languages </b>
       </h4>
       <div align="center">
         {LanguagesData.map((item, index) => (
           <a href={item.href} key={index} target="_blank" rel="noreferrer">
-            <img src={item.src} alt={item.alt} width="40" height="40" />
+            <img
+              src={item.src}
+              alt={item.alt}
+              style={{ height: '5em', width: '5em' }}
+            />
           </a>
         ))}
       </div>
 
-      <h4 align="center">
+      <h4 align="center" style={{ fontSize: '3em' }}>
         <b>Front-End</b>
       </h4>
       <div align="center">
         {FrontendData.map((item, index) => (
           <a href={item.href} key={index} target="_blank" rel="noreferrer">
-            <img src={item.src} alt={item.alt} width="40" height="40" />
+            <img
+              src={item.src}
+              alt={item.alt}
+              style={{ height: '5em', width: '5em' }}
+            />
           </a>
         ))}
       </div>
-      <h4 align="center">
+      <h4 align="center" style={{ fontSize: '3em' }}>
         <b>Back-End</b>
       </h4>
       <div align="center">
         {BackendData.map((item, index) => (
           <a href={item.href} key={index} target="_blank" rel="noreferrer">
-            <img src={item.src} alt={item.alt} width="40" height="40" />
+            <img
+              src={item.src}
+              alt={item.alt}
+              style={{ height: '5em', width: '5em' }}
+            />
           </a>
         ))}
       </div>
-      <h4 align="center">
+      <h4 align="center" style={{ fontSize: '3em' }}>
         <b>Tools</b>
       </h4>
       <div align="center">
         {ToolsData.map((item, index) => (
           <a href={item.href} key={index} target="_blank" rel="noreferrer">
-            <img src={item.src} alt={item.alt} width="40" height="40" />
+            <img
+              src={item.src}
+              alt={item.alt}
+              style={{ height: '5em', width: '5em' }}
+            />
           </a>
         ))}
       </div>
