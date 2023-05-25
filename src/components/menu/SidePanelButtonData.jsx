c0e31a3d5644d1ec69bccd22081c7fe2480c2b60
CJ <cliftonjones.87@gmail.com>
2023-05-25 16:27:06 -0400

    chore: commented out links for tech socials that arent being maintained

--- src/components/menu/SidePanelButtonData.jsx
+++ src/components/menu/SidePanelButtonData.jsx
@@ -5,30 +5,30 @@ const SidePanelButtonData = [
     href: 'https://www.linkedin.com/in/cjones1827/',
     iconClassName: 'pi pi-linkedin px-2',
   },
-  {
-    buttonClassName: 'youtube p-0',
-    buttonText: 'Youtube',
-    href: 'https://www.youtube.com/channel/UCIgDLvCc7d6pX1LrhgwLZMw',
-    iconClassName: 'pi pi-youtube px-2',
-  },
-  {
-    buttonClassName: 'instagram p-0',
-    buttonText: 'Instagram',
-    href: 'https://www.instagram.com/seejonesengineer/',
-    iconClassName: 'pi pi-instagram px-2',
-  },
-  {
-    buttonClassName: 'facebook p-0',
-    buttonText: 'Facebook',
-    href: 'https://www.facebook.com/seejonesengineer',
-    iconClassName: 'pi pi-facebook px-2',
-  },
-  {
-    buttonClassName: 'slack p-0',
-    buttonText: 'Slack',
-    href: 'https://seejonesengineer.slack.com/archives/D037H4J82TU',
-    iconClassName: 'pi pi-slack px-2',
-  },
+  // {
+  //   buttonClassName: 'youtube p-0',
+  //   buttonText: 'Youtube',
+  //   href: 'https://www.youtube.com/channel/UCIgDLvCc7d6pX1LrhgwLZMw',
+  //   iconClassName: 'pi pi-youtube px-2',
+  // },
+  // {
+  //   buttonClassName: 'instagram p-0',
+  //   buttonText: 'Instagram',
+  //   href: 'https://www.instagram.com/seejonesengineer/',
+  //   iconClassName: 'pi pi-instagram px-2',
+  // },
+  // {
+  //   buttonClassName: 'facebook p-0',
+  //   buttonText: 'Facebook',
+  //   href: 'https://www.facebook.com/seejonesengineer',
+  //   iconClassName: 'pi pi-facebook px-2',
+  // },
+  // {
+  //   buttonClassName: 'slack p-0',
+  //   buttonText: 'Slack',
+  //   href: 'https://seejonesengineer.slack.com/archives/D037H4J82TU',
+  //   iconClassName: 'pi pi-slack px-2',
+  // },
   //   {
   //     buttonClassName: 'whatsapp p-0',
   //     buttonText: 'WhatsApp',
@@ -47,12 +47,12 @@ const SidePanelButtonData = [
     href: 'https://github.com/cjones-87',
     iconClassName: 'pi pi-github px-2',
   },
-  {
-    buttonClassName: 'tiktok p-0',
-    buttonText: 'TikTok',
-    href: 'https://www.tiktok.com/@seejonesengineer',
-    iconClassName: 'fab fa-tiktok px-2',
-  },
+  // {
+  //   buttonClassName: 'tiktok p-0',
+  //   buttonText: 'TikTok',
+  //   href: 'https://www.tiktok.com/@seejonesengineer',
+  //   iconClassName: 'fab fa-tiktok px-2',
+  // },
   {
     buttonClassName: 'coffee p-0',
     buttonText: 'Buy Me A Coffee',
