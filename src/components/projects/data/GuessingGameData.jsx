ef9990d0697e48aee8988e73a8e4553ee3f37ed6
CJ <cliftonjones.87@gmail.com>
2023-05-26 22:11:02 -0400

    refactor: relocated data to centralized location for improved data management

---
+++ src/components/projects/data/GuessingGameData.jsx
@@ -0,0 +1,21 @@
+import { GuessingGamePic } from '../../../photos/PhotoExports';
+
+const GuessingGameData = [
+  {
+    alt: 'Guessing Game Photo',
+    img: GuessingGamePic,
+    paragraph: `I developed a simple Guessing Game application using a combination of HTML, CSS, and client-side JavaScript, with a focus on introducing DOM manipulation. The Minimum Viable Product for this game was a basic guessing game, where the user had limited guesses to guess a number between 1 and 100, with the aid of a hint button and a reset button. This game was designed to be an instructional introduction to DOM manipulation, and as such, no future goals were planned for this application.`,
+  },
+  {
+    alt: 'Guessing Game Photo',
+    img: GuessingGamePic,
+    paragraph: `To build this game, I integrated HTML5 and CSS3 for the visual design and layout, while client-side JavaScript was utilized to implement the game logic and data for gameplay. I created all the code and design elements myself, from scratch, as a solo project. However, while building this application, Throughout this project, I was learning how to manipulate the DOM for the first time, which was a bit challenging but fun.`,
+  },
+  {
+    alt: 'Guessing Game Photo',
+    img: GuessingGamePic,
+    paragraph: `Overall, this project helped me gain valuable experience in the DOM manipulation field and gave me a basic understanding of how to develop a small game application. I utilized HTML5, CSS3, and client-side JavaScript to create a minimum viable product that included all necessary game elements while learning the necessary techniques to manipulate the DOM. In conclusion, the Guessing Game application was a successful endeavor, and I am proud to have completed it as my first solo project.`,
+  },
+];
+
+export default GuessingGameData;
