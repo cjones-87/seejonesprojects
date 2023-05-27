1e526f29d73bd2b6ae746630c28bafdcde14fbb9
CJ <cliftonjones.87@gmail.com>
2023-05-26 22:16:44 -0400

    refactor: relocated data to centralized location for improved data management

---
+++ src/components/projects/data/HypnotiqData.jsx
@@ -0,0 +1,33 @@
+import {
+  BabyJams,
+  ClassicalJams,
+  JazzJams,
+} from '../../../photos/PhotoExports';
+
+const HypnotiqData = [
+  {
+    alt: 'Hypnotiq Youtube Video',
+    iframe: 'https://www.youtube.com/embed/Z-RTFL_FYu8',
+    paragraph: `I created a mobile application called Hypnotiq. It is designed and constructed solely by myself, using React Native Expo for the front-end. Hypnotiq's minimum viable product (MVP) presents users with a list of sound categories to choose from, allowing them to select different options within each category to facilitate studying and sleep habits. This app features five related facts for each of the five senses and an additional random fact for each category.`,
+  },
+  {
+    alt: 'Hypnotiq Youtube Video',
+    iframe: 'https://www.youtube.com/embed/Z-RTFL_FYu8',
+    img: BabyJams,
+    paragraph: `My stretch goals for this app include making it more inclusive by adding more music genres and including the features from Hypnotiq 1.0 in its next release version. Additionally, I plan to give users the option to create, save, and share playlists with others and to allow users to comment directly on shared playlists.`,
+  },
+  {
+    alt: 'Hypnotiq Youtube Video',
+    iframe: 'https://www.youtube.com/embed/Z-RTFL_FYu8',
+    img: ClassicalJams,
+    paragraph: `As the sole developer of this website, I contributed to all aspects of its creation, from concept to deployment. Technologies used in Hypnotiq include React, React Native, Expo, CSS3, and was deployed through Heroku. This project was challenging, as I had no prior experience with React Native Expo. However, I often found mobile features to be more intuitive, and I enjoyed learning about the different ways mobile phones navigate screens and display data compared to web sites & desktop apps.`,
+  },
+  {
+    alt: 'Hypnotiq Youtube Video',
+    iframe: 'https://www.youtube.com/embed/Z-RTFL_FYu8',
+    img: JazzJams,
+    paragraph: `Overall, Hypnotiq was a fun learning experience as I am passionate about mobile technology and its accessibility. As more companies and sites move towards mobile access, I believe mobile technology is essential in our daily lives. The challenges I faced included learning a new technology, but I enjoyed the experience of conceptualizing and implementing mobile features.`,
+  },
+];
+
+export default HypnotiqData;
