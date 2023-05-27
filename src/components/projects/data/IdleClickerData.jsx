729fc1ff4cf3a842d384f0ba4a70061b1afcb7ce
CJ <cliftonjones.87@gmail.com>
2023-05-26 22:20:46 -0400

    refactor: relocated data to centralized location for improved data management

---
+++ src/components/projects/data/IdleClickerData.jsx
@@ -0,0 +1,21 @@
+import { IdleClickerPic } from '../../../photos/PhotoExports';
+
+const IdleClickerData = [
+  {
+    alt: 'Idle Clicker Photo',
+    img: IdleClickerPic,
+    paragraph: `I am proud to present my Idle Clicker game, which was constructed and designed using HTML5 and CSS3. The game logic and data were implemented using client-side JavaScript. The Minimum Viable Product (MVP) goal for this idle clicker game is for players to click on a coffee emoji to increment a counter that displays the quantity of coffee they possess. They can use coffee to purchase coffee-producing devices such as percolators and espresso machines. The player accumulates coffee passively based on the number and type of coffee producers in stock. The price of each producer steadily increases, ensuring that the game remains interesting even after coffee production has been automated. The player can see their current coffee count, and new producers are unlocked successively as they accumulate more coffee.`,
+  },
+  {
+    alt: 'Idle Clicker Photo',
+    img: IdleClickerPic,
+    paragraph: `As for stretch goals, one future plan for this idle clicker game is to incorporate the ability to save data. This would allow players to pause progress and return to gameplay at a later time. Another goal is to add the ability to sell back producers for a portion of their original cost. The most significant goal for Idle Clicker 2.0 is to add unlockable upgrade systems and producer/production multipliers.`,
+  },
+  {
+    alt: 'Idle Clicker Photo',
+    img: IdleClickerPic,
+    paragraph: `I was the sole contributor to this project, completed during the early days of my understanding of DOM manipulation. While it was an enlightening endeavor, understanding how to update a page dynamically in real-time proved to be the biggest challenge I faced in completing this idle clicker game. Nevertheless, it was an excellent opportunity to gain a better understanding of how to make a website dynamic and paved the way for truly understanding some of the intricacies of React. Idle Clicker was an exciting project that provided a great learning opportunity.`,
+  },
+];
+
+export default IdleClickerData;
