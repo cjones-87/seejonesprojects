86569cabdec9562442399c8c3d5ef904b36522c2
CJ <cliftonjones.87@gmail.com>
2023-05-26 22:13:17 -0400

    refactor: relocated data to centralized location for improved data management

---
+++ src/components/projects/data/Hypnotiq2Data.jsx
@@ -0,0 +1,24 @@
+const Hypnotiq2Data = [
+  {
+    alt: 'Hypnotiq 2 Youtube Video',
+    iframe: 'https://www.youtube.com/embed/GFI7VYjWjaY',
+    paragraph: `Hypnotiq 2.0 is a music player app developed by myself. This app uses the latest industry technologies such as Javascript, React, React Native, Expo, CSS, Firebase, and Cloud Firestore. The app allows users to create, edit, and delete music playlists that persist through app reloads, and load music from their local device storage. Users can also stream audio to help with sleep and study patterns.`,
+  },
+  {
+    alt: 'Hypnotiq 2 Youtube Video',
+    iframe: 'https://www.youtube.com/embed/GFI7VYjWjaY',
+    paragraph: `In future iterations, I plan to restructure the database design to optimize functionality. The current database structure posed some challenges, but I was able to implement some of the stretch goals from Hypnotiq, such as presenting users with audio from different genres and enabling them to create and manage personal playlists.`,
+  },
+  {
+    alt: 'Hypnotiq 2 Youtube Video',
+    iframe: 'https://www.youtube.com/embed/GFI7VYjWjaY',
+    paragraph: `Another key stretch goal for the upcoming iteration is to provide users with the option to share their playlists with others and comment on shared playlists. The ability to direct message other users will also be added, but only for admins and artists whose work has been shared.`,
+  },
+  {
+    alt: 'Hypnotiq 2 Youtube Video',
+    iframe: 'https://www.youtube.com/embed/GFI7VYjWjaY',
+    paragraph: `One of the significant challenges in developing Hypnotiq 2.0 was maintaining audio playback state and other related considerations. To achieve seamless audio playback and allow users complete control, I had to keep track of multiple factors such as audio loading, currently playing audio, from playlist or general library, current audio position, last audio position, and more. Hypnotiq 2.0 showcases my expertise and passion for utilizing the latest industry technologies to develop innovative software solutions that enhance user experiences.`,
+  },
+];
+
+export default Hypnotiq2Data;
