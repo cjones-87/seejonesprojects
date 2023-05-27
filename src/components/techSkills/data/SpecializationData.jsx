59c185112d66ffe96bb65913c20f9aec5ef3c84c
CJ <cliftonjones.87@gmail.com>
2023-05-26 20:49:21 -0400

    refactor: relocated data to centralized location for improved data management

---
+++ src/components/techSkills/data/SpecializationData.jsx
@@ -0,0 +1,30 @@
+export const WebDevelopmentData = [
+  {
+    alt: 'React',
+    href: 'https://reactjs.org/',
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg',
+  },
+  {
+    alt: 'Javascript',
+    href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg',
+  },
+];
+
+export const MobileDevelopmentData = [
+  {
+    alt: 'React',
+    href: 'https://reactjs.org/',
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg',
+  },
+  {
+    alt: 'React Native Expo',
+    href: 'https://docs.expo.dev/',
+    src: 'https://i.imgur.com/LNiblbi.png',
+  },
+  {
+    alt: 'Javascript',
+    href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg',
+  },
+];
