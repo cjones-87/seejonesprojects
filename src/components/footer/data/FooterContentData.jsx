2003fb8cc872101c9c5533c454d09d07cc07c636
CJ <cliftonjones.87@gmail.com>
2023-05-26 21:08:23 -0400

    refactor: relocated data to centralized location for improved data management

---
+++ src/components/footer/data/FooterContentData.jsx
@@ -0,0 +1,40 @@
+const FooterContentData = [
+  {
+    className: 'fa fa-file-pdf fa-lg white-text mr-md-5 mr-3 fa-2x',
+    href: '/CJsSoftwareEngineeringResume.pdf',
+  },
+  {
+    className: 'fa fa-at fa-lg white-text mr-md-5 mr-3 fa-2x',
+    href: 'mailto:cj@seejonesengineer.com',
+  },
+  {
+    className: 'fa fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x',
+    href: 'https://www.linkedin.com/in/cjones1827/',
+  },
+  {
+    className: 'fa fa-github fa-lg white-text mr-md-5 mr-3 fa-2x',
+    href: 'https://www.github.com/cjones-87/',
+  },
+  // {
+  //   className: 'fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x',
+  //   href: 'https://www.instagram.com/seejonesengineer/',
+  // },
+  {
+    className: 'fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x',
+    href: 'https://twitter.com/cjonesengineer1',
+  },
+  // {
+  //   className: 'fa fa-youtube fa-lg white-text mr-md-5 mr-3 fa-2x',
+  //   href: 'https://www.youtube.com/channel/UCIgDLvCc7d6pX1LrhgwLZMw',
+  // },
+  // {
+  //   className: 'fab fa-tiktok fa-lg white-text mr-md-5 mr-3 fa-2x',
+  //   href: 'https://www.tiktok.com/@seejonesengineer',
+  // },
+  // {
+  //   className: 'fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x',
+  //   href: 'https://www.facebook.com/seejonesengineer/',
+  // },
+];
+
+export default FooterContentData;
