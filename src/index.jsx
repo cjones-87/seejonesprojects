13967dcb3e237804c43fdf2cc144ea4e7fa0d4a4
CJ <cliftonjones.87@gmail.com>
2023-06-09 13:10:41 -0400

    refactor: updating to add light/dark mode context at highest order component for sitewide application

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-
 import App from './App';
+import LightDarkThemeContext from '../misc/context/LightDarkThemeContext';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <LightDarkThemeContext>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </LightDarkThemeContext>
   </React.StrictMode>,
   document.getElementById('root')
 );
