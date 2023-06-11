225899a67fb32139dd1028c466c6d4fc8ebe08aa
CJ <cliftonjones.87@gmail.com>
2023-06-11 00:59:55 -0400

    feature: added project title for header

--- src/components/projects/IYKYK/IYKYK.jsx
+++ src/components/projects/IYKYK/IYKYK.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
 import IndividualProject from '../IndividualProject';
 import IYKYKData from '../data/IYKYKData';
 import useWindowDimensions from '../../../../misc/customHooks/useWindowDimensions';
 
 const IYKYK = () => {
   const { height, width } = useWindowDimensions();
-  return <IndividualProject data={IYKYKData} height={height} width={width} />;
+  return (
+    <IndividualProject
+      data={IYKYKData}
+      height={height}
+      title="IYKYK"
+      width={width}
+    />
+  );
 };
 
 export default IYKYK;
