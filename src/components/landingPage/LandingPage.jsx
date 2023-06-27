3a6693c222fa44a7defc01c90783fe6529ac0f1a
CJ <cliftonjones.87@gmail.com>
2023-06-26 23:40:44 -0400

    refactor: removed additional info and relocated to home page

--- src/components/landingPage/LandingPage.jsx
+++ src/components/landingPage/LandingPage.jsx
@@ -1,12 +1,5 @@
 import { useContext, useEffect } from 'react';
-import {
-  BackendData,
-  FrontendData,
-  LanguagesData,
-  ToolsData,
-} from '../techSkills/data/TechSkillsData';
 import { Button } from 'primereact/button';
-import LazyLoadImage from '../lazy/LazyLoadImage';
 import Typewriter from 'typewriter-effect';
 import useWindowDimensions from '../../../misc/customHooks/useWindowDimensions';
 import OpenUp from '../../sounds/OpenUp.mp3';
@@ -46,8 +39,7 @@ const LandingPage = () => {
         background: darkTheme
           ? 'radial-gradient(#434343, rgba(0, 0, 0, 1))'
           : 'radial-gradient(#1a1a1a, rgba(163, 163, 163, 1))',
-        height: height < 900 ? '230vh' : '125vh',
-        width: '100vw',
+        width,
       }}
     >
       <h1
@@ -104,155 +96,6 @@ const LandingPage = () => {
           textShadow: '2px 2px 2px indigo',
         }}
       />
-
-      <div
-        id="sectionA"
-        style={{
-          backgroundColor: darkTheme
-            ? 'rgb(128, 128, 128, 0.3)'
-            : 'rgb(75, 0, 130, 0.3)',
-          color: 'whitesmoke',
-          fontSize: '4em',
-          width: '100vw',
-        }}
-      >
-        I am a freelance software engineer, passionate about learning &
-        implementing new technologies. I am proficient in writing clean,
-        efficient, DRY implementations. I am a fastidious engineer and
-        perspicacious problem solver who can accomplish anything I set my mind
-        to.
-      </div>
-
-      <div id="sectionC" style={{ color: 'whitesmoke' }}>
-        <div align="center" id="sectionC1" style={{ width: width / 3 }}>
-          <h4 align="center" style={{ fontSize: '3em' }}>
-            Proficient Languages
-          </h4>
-          {LanguagesData.map((data, index) => (
-            <a href={data.href} key={index} rel="noreferrer" target="_blank">
-              <LazyLoadImage
-                alt={data.alt}
-                height="40"
-                key={index}
-                src={data.src}
-                width="40"
-              />
-            </a>
-          ))}
-        </div>
-
-        <div align="center" id="sectionC2" style={{ width: width / 3 }}>
-          <h4 align="center" style={{ fontSize: '3em' }}>
-            Familiar (Read-Only) Languages
-          </h4>
-          <a href="https://www.python.org" target="_blank" rel="noreferrer">
-            <LazyLoadImage
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
-              alt="python"
-              width="40"
-              height="40"
-            />
-          </a>
-        </div>
-
-        <div align="center" id="sectionC1" style={{ width: width / 3 }}>
-          <h4 align="center" style={{ fontSize: '3em' }}>
-            Knowledgeable Languages
-          </h4>
-          <a
-            href="https://www.typescriptlang.org/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <LazyLoadImage
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
-              alt="typescript"
-              width="40"
-              height="40"
-            />
-          </a>
-        </div>
-      </div>
-
-      <div id="sectionC" style={{ color: 'whitesmoke', width: '100vw' }}>
-        <div className="sectionC2" style={{ width: width / 3 }}>
-          <h4 align="center" style={{ fontSize: '3em' }}>
-            Front-End
-          </h4>
-          {FrontendData.map((data, index) => (
-            <a href={data.href} key={index} rel="noreferrer" target="_blank">
-              <LazyLoadImage
-                alt={data.alt}
-                height="40"
-                key={index}
-                src={data.src}
-                width="40"
-              />
-            </a>
-          ))}
-        </div>
-
-        <div className="sectionC1" style={{ width: width / 3 }}>
-          <h4 align="center" style={{ fontSize: '3em' }}>
-            Tools
-          </h4>
-
-          {ToolsData.map((data, index) => (
-            <a href={data.href} key={index} rel="noreferrer" target="_blank">
-              <LazyLoadImage
-                alt={data.alt}
-                height="40"
-                key={index}
-                src={data.src}
-                width="40"
-              />
-            </a>
-          ))}
-        </div>
-        <div className="sectionC2" style={{ width: width / 3 }}>
-          <h4 align="center" style={{ fontSize: '3em' }}>
-            Back-End
-          </h4>
-
-          {BackendData.map((data, index) => (
-            <a href={data.href} key={index} rel="noreferrer" target="_blank">
-              <LazyLoadImage
-                alt={data.alt}
-                height="40"
-                key={index}
-                src={data.src}
-                width="40"
-              />
-            </a>
-          ))}
-        </div>
-      </div>
-
-      <div
-        id="sectionC"
-        style={{
-          width: '100vw',
-        }}
-      >
-        <div
-          className="sectionC1"
-          style={{ fontSize: '3em', width: width / 3 }}
-        >
-          Web Apps
-        </div>
-        <div
-          className="sectionC2"
-          style={{ fontSize: '3em', width: width / 3 }}
-        >
-          Mobile Apps
-        </div>
-        <div
-          className="sectionC1"
-          style={{ fontSize: '3em', width: width / 3 }}
-        >
-          Websites
-        </div>
-      </div>
     </div>
   );
 };
