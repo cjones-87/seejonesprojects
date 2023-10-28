5f1f033729576cbeaf18ee2fb1bc83bb7681a2b1
CJ <cliftonjones.87@gmail.com>
2023-10-27 22:15:17 -0400

    refactor: changed data reference format and added additional fields

--- src/components/home/data/GithubActivityData.jsx
+++ src/components/home/data/GithubActivityData.jsx
@@ -1,15 +1,19 @@
 const GithubActivityData = [
   {
+    alt: '2021 Github Skyline',
     href: 'https://skyline.github.com/cjones-87/2021',
-    src: 'https://www.youtube.com/embed/BAhz7BLA3rA',
+    imageSrc: 'https://i.imgur.com/IshzFqO.gif',
+    iframeSrc: 'https://www.youtube.com/embed/BAhz7BLA3rA',
   },
   {
+    alt: '2022 Github Skyline',
     href: 'https://skyline.github.com/cjones-87/2022',
-    src: 'https://www.youtube.com/embed/fwvVifpo93o',
+    iframeSrc: 'https://www.youtube.com/embed/fwvVifpo93o',
   },
   {
+    alt: '2023 Github Skyline',
     href: 'https://skyline.github.com/cjones-87/2023',
-    src: 'https://www.youtube.com/embed/-jgXmlOViws',
+    iframeSrc: 'https://www.youtube.com/embed/-jgXmlOViws',
   },
 ];
 
