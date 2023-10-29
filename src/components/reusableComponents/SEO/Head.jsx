9b78df2c0a65017d20a957143e358f8744a2fb6c
CJ <cliftonjones.87@gmail.com>
2023-10-29 17:36:50 -0400

    refactor: refactored head component for improved seo and social media metadata handling

--- src/components/reusableComponents/SEO/Head.jsx
+++ src/components/reusableComponents/SEO/Head.jsx
@@ -1,10 +1,43 @@
 import { Helmet } from 'react-helmet';
+import { useLocation } from 'react-router-dom';
+import site from './site';
 
-const Head = ({ description, title }) => {
+const Head = ({ description, image, title }) => {
+  const { pathname } = useLocation();
+
+  const {
+    defaultTitle,
+    defaultDescription,
+    siteUrl,
+    defaultImage,
+    twitterUsername,
+  } = site.siteMetadata;
+
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    image: `${siteUrl}${image || defaultImage}`,
+    url: `${siteUrl}${pathname}`,
+  };
+
   return (
-    <Helmet>
-      <title>{title}</title>
-      <meta name='description' content={description} />
+    <Helmet description={seo.description} image={seo.image} title={seo.title}>
+      <html lang='en' />
+
+      <meta name='description' content={seo.description} />
+      <meta name='image' content={seo.image} />
+
+      <meta property='og:title' content={seo.title} />
+      <meta property='og:description' content={seo.description} />
+      <meta property='og:image' content={seo.image} />
+      <meta property='og:url' content={seo.url} />
+      <meta property='og:type' content='website' />
+
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:creator' content={twitterUsername} />
+      <meta name='twitter:title' content={seo.title} />
+      <meta name='twitter:description' content={seo.description} />
+      <meta name='twitter:image' content={seo.image} />
     </Helmet>
   );
 };
