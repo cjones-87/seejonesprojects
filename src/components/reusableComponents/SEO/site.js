a43857b3f0ff0e5926f4b2af89c58b90b7915ad4
CJ <cliftonjones.87@gmail.com>
2023-10-29 17:30:56 -0400

    feature: added an object to contain site default metadata

---
+++ src/components/reusableComponents/SEO/site.js
@@ -0,0 +1,13 @@
+const site = {
+  siteMetadata: {
+    defaultTitle:
+      'CJ Jones, Software Engineer: Exploring Excellence in Software Engineering',
+    defaultDescription:
+      'Welcome to the digital realm curated by CJ Jones, a passionate software engineer. Dive into a world where innovation meets usability, and explore a collection of projects, experiences, and insights that define the journey in software engineering excellence. From cutting-edge solutions to educational pursuits, join me in navigating the exciting landscape of technology. Where creativity meets code, possibilities unfold.',
+    siteUrl: 'https://www.seejonesengineer.com/',
+    defaultImage: '/OG.png',
+    twitterUsername: 'cjonesengineer1',
+  },
+};
+
+export default site;
