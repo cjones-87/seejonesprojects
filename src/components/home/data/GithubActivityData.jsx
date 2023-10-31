0784b1044bcb7351ca9f6db6bca7d5d47f37ccb7
CJ <cliftonjones.87@gmail.com>
2023-10-30 20:00:39 -0400

    chore: updated alt attributes for seo optimization

--- src/components/home/data/GithubActivityData.jsx
+++ src/components/home/data/GithubActivityData.jsx
@@ -1,17 +1,17 @@
 const GithubActivityData = [
   {
-    alt: '2021 Github Skyline',
+    alt: 'CJ Jones, Software Engineer - 2021 Github Skyline Image',
     href: 'https://skyline.github.com/cjones-87/2021',
     imageSrc: 'https://i.imgur.com/IshzFqO.gif',
     iframeSrc: 'https://www.youtube.com/embed/BAhz7BLA3rA',
   },
   {
-    alt: '2022 Github Skyline',
+    alt: 'CJ Jones, Software Engineer - 2022 Github Skyline Image',
     href: 'https://skyline.github.com/cjones-87/2022',
     iframeSrc: 'https://www.youtube.com/embed/fwvVifpo93o',
   },
   {
-    alt: '2023 Github Skyline',
+    alt: 'CJ Jones, Software Engineer - 2023 Github Skyline Image',
     href: 'https://skyline.github.com/cjones-87/2023',
     iframeSrc: 'https://www.youtube.com/embed/-jgXmlOViws',
   },
