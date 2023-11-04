759c2960f8262f24b5c8d84ecfa8f0345d385450
CJ <cliftonjones.87@gmail.com>
2023-11-03 22:55:45 -0400

    chore: updated conditional logic for readability & maintainability

--- src/components/menu/SidePanelMenu.jsx
+++ src/components/menu/SidePanelMenu.jsx
@@ -43,15 +43,15 @@ const SidePanelMenu = () => {
           icons={''}
           onHide={onHide}
           style={{
-            background: !darkMode
-              ? 'radial-gradient(#1a1a1a, rgba(163, 163, 163, 1))'
-              : 'radial-gradient(#434343, rgba(0, 0, 0, 1))',
+            background: darkMode
+              ? 'radial-gradient(#434343, rgba(0, 0, 0, 1))'
+              : 'radial-gradient(#1a1a1a, rgba(163, 163, 163, 1))',
           }}
           visible={showSidebar}
         >
           <h3
             style={{
-              color: !darkMode ? 'whitesmoke' : '#ba68c8',
+              color: darkMode ? '#ba68c8' : 'whitesmoke',
               textAlign: 'center',
               textShadow: '2px 2px 2px indigo',
             }}
@@ -79,10 +79,10 @@ const SidePanelMenu = () => {
               }
               onClick={onOpen}
               style={{
-                background: !darkMode
-                  ? 'radial-gradient(#434343, rgba(0, 0, 0, 1))'
-                  : 'radial-gradient(#1a1a1a, rgba(163, 163, 163, 1))',
-                color: !darkMode ? '#ba68c8' : 'whitesmoke',
+                background: darkMode
+                  ? 'radial-gradient(#1a1a1a, rgba(163, 163, 163, 1))'
+                  : 'radial-gradient(#434343, rgba(0, 0, 0, 1))',
+                color: darkMode ? 'whitesmoke' : '#ba68c8',
                 filter: 'drop-shadow(0px 2px 4px rgba(75, 0, 130, 1))',
                 height: '2.5em',
                 textShadow: '2px 2px 2px indigo',
