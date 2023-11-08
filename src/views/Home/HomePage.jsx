59ab2596d8b2dfb23cd6d6b22c9c980123d5ba9a
CJ <cliftonjones.87@gmail.com>
2023-11-07 23:23:31 -0500

    refactor: added new view for home page for conditionally rendering previous landing page & current home page as one route

---
+++ src/views/Home/HomePage.jsx
@@ -0,0 +1,30 @@
+import { lazy, useState } from 'react';
+
+const LandingPage = lazy(() =>
+  import('../../components/landingPage/LandingPage')
+);
+const Home = lazy(() => import('../../components/home/Home'));
+const Head = lazy(() => import('../../components/reusableComponents/SEO/Head'));
+
+const HomePage = () => {
+  const [showHomePage, setShowHomePage] = useState(false);
+
+  const handleEntranceClick = () => setShowHomePage(true);
+
+  return (
+    <div>
+      <Head
+        title='How to Build a Website: CJ Jones - SeeJonesEngineer'
+        description='Hire CJ Jones, a skilled software engineer at SeeJonesEngineer, for expert guidance on building your website. Unlock a seamless website development experience by bringing on board an experienced professional dedicated to realizing & expanding your vision.'
+      />
+
+      {showHomePage ? (
+        <Home />
+      ) : (
+        <LandingPage handleEntranceClick={handleEntranceClick} />
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
